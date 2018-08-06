fbb4da53fa3df84e5684d21672cd2c4e6751fbf8
Joe Conway <joeconway3@gmail.com>
2018-08-06 11:24:51 -0400

    Update pubspec for release (#55)
    * update pubspec for release
    
    * remove prerelease env var
    
    * stable
    
    * disable coverage

--- ci/script.sh
+++ ci/script.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -e
 
-export DART_VM_OPTIONS=--preview-dart-2
-
 pub run test -j 1 -r expanded
 
-export DART_VM_OPTIONS=""
-
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  pub global activate -sgit https://github.com/stablekernel/codecov_dart.git
-  dart_codecov_generator --report-on=lib/ --verbose --no-html
-fi
+# if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+#  pub global activate -sgit https://github.com/stablekernel/codecov_dart.git
+#  dart_codecov_generator --report-on=lib/ --verbose --no-html
+# fi
