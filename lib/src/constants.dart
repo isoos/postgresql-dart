35c6b85368be87061e167143838d1b7f6c6db798
Istvan Soos <istvan.soos@gmail.com>
2019-10-30 18:10:20 +0100

    Remove unnecessary const.

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,7 +1,7 @@
 class UTF8ByteConstants {
-  static const user = const <int>[117, 115, 101, 114, 0];
-  static const database = const <int>[100, 97, 116, 97, 98, 97, 115, 101, 0];
-  static const clientEncoding = const <int>[
+  static const user = <int>[117, 115, 101, 114, 0];
+  static const database = <int>[100, 97, 116, 97, 98, 97, 115, 101, 0];
+  static const clientEncoding = <int>[
     99,
     108,
     105,
@@ -19,6 +19,6 @@ class UTF8ByteConstants {
     103,
     0
   ];
-  static const utf8 = const <int>[85, 84, 70, 56, 0];
-  static const timeZone = const <int>[84, 105, 109, 101, 90, 111, 110, 101, 0];
+  static const utf8 = <int>[85, 84, 70, 56, 0];
+  static const timeZone = <int>[84, 105, 109, 101, 90, 111, 110, 101, 0];
 }
