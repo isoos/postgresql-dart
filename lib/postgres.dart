6178957b159e563e73eb710c569d11928f8b8c71
Kilian Schulte <schultek@in.tum.de>
2021-04-13 22:18:09 +0200

    export PostgresTextEncoder
    Can we do this please? I need to use the `.convert()` method directly in my project.

--- lib/postgres.dart
+++ lib/postgres.dart
@@ -4,4 +4,5 @@ export 'src/connection.dart';
 export 'src/execution_context.dart';
 export 'src/models.dart';
 export 'src/substituter.dart';
+export 'src/text_codec.dart';
 export 'src/types.dart';
