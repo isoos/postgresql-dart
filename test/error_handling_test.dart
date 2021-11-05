7625b2df9994a6dedba83f4bea017f6b921f5a0b
David Martos <davidmartos96@gmail.com>
2021-11-05 09:53:53 +0100

    Execute

--- test/error_handling_test.dart
+++ test/error_handling_test.dart
@@ -12,12 +12,25 @@ void main() {
     await conn.open();
     addTearDown(() async => conn.close());
 
-    // Root connection
+    // Root connection query
     try {
       await conn.query('SELECT hello');
       fail('Should not reach');
     } catch (e, st) {
       expect(e.toString(), contains('column "hello" does not exist'));
+      expect(
+        st.toString(),
+        contains('postgresql-dart/test/error_handling_test.dart'),
+      );
+    }
+
+    // Root connection execute
+    try {
+      await conn.execute('DELETE FROM hello');
+      fail('Should not reach');
+    } catch (e, st) {
+      print(e);
+      expect(e.toString(), contains('relation "hello" does not exist'));
       expect(
         st.toString(),
         contains('postgresql-dart/test/error_handling_test.dart'),
