66c8c5d30242d1d33cfd1f4e498a068655a80aa3
pedropastor <>
2021-11-27 12:11:51 +0100

    Add clear type and auth scheme

--- lib/src/auth/auth.dart
+++ lib/src/auth/auth.dart
@@ -3,10 +3,11 @@ import 'package:sasl_scram/sasl_scram.dart';
 
 import '../../postgres.dart';
 import '../server_messages.dart';
+import 'clearText_authenticator.dart';
 import 'md5_authenticator.dart';
 import 'sasl_authenticator.dart';
 
-enum AuthenticationScheme { MD5, SCRAM_SHA_256 }
+enum AuthenticationScheme { MD5, SCRAM_SHA_256, CLEAR }
 
 abstract class PostgresAuthenticator {
   static String? name;
@@ -27,6 +28,8 @@ PostgresAuthenticator createAuthenticator(PostgreSQLConnection connection,
           username: connection.username, password: connection.password);
       return PostgresSaslAuthenticator(
           connection, ScramAuthenticator('SCRAM-SHA-256', sha256, credentials));
+    case AuthenticationScheme.CLEAR:
+      return ClearAuthenticator(connection);
     default:
       throw PostgreSQLException("Authenticator wasn't specified");
   }
