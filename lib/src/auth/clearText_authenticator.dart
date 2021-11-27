529ed4c772dee65308343f1f97bdd9caa25454af
pedropastor <>
2021-11-27 12:11:57 +0100

    Create clearText_authenticator.dart

---
+++ lib/src/auth/clearText_authenticator.dart
@@ -0,0 +1,33 @@
+import 'package:buffer/buffer.dart';
+
+import '../../postgres.dart';
+import '../client_messages.dart';
+import '../server_messages.dart';
+import '../utf8_backed_string.dart';
+import 'auth.dart';
+
+class ClearAuthenticator extends PostgresAuthenticator{
+  ClearAuthenticator(PostgreSQLConnection connection) : super(connection);
+
+  @override
+  void onMessage(AuthenticationMessage message) {
+    final authMessage = ClearMessage(connection.password!);
+    connection.socket!.add(authMessage.asBytes());
+  }
+
+}
+class ClearMessage extends ClientMessage {
+  UTF8BackedString? _authString;
+
+  ClearMessage(String password) {
+    _authString = UTF8BackedString(password);
+  }
+
+  @override
+  void applyToBuffer(ByteDataWriter buffer) {
+    buffer.writeUint8(ClientMessage.PasswordIdentifier);
+    final length = 5 + _authString!.utf8Length;
+    buffer.writeUint32(length);
+    _authString!.applyToBuffer(buffer);
+  }
+}
