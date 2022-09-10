21a1672f2c42c8b8f88e3841ee11f620f26102a7
osaxma <46427323+osaxma@users.noreply.github.com>
2022-09-10 15:55:02 +0300

    export replication mode in postgres library

--- lib/postgres.dart
+++ lib/postgres.dart
@@ -3,5 +3,6 @@ library postgres;
 export 'src/connection.dart';
 export 'src/execution_context.dart';
 export 'src/models.dart';
+export 'src/replication.dart' show ReplicationMode;
 export 'src/substituter.dart';
 export 'src/types.dart';
