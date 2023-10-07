fa03d6871dfcb98d3b7d43752115831cb6586d22
Istvan Soos <istvan.soos@agilord.com>
2023-10-07 10:12:26 +0200

    Refactor docker initialization.

--- test/docker.dart
+++ test/docker.dart
@@ -4,67 +4,78 @@ import 'package:docker_process/containers/postgres.dart';
 import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
-const _kContainerName = 'postgres-dart-test';
-
 void usePostgresDocker() {
+  final port = 5432;
+  final containerName = 'postgres-dart-test-$port';
+
   setUpAll(() async {
-    final isRunning = await _isPostgresContainerRunning();
-    if (isRunning) {
-      return;
-    }
-
-    final configPath = p.join(Directory.current.path, 'test', 'pg_configs');
-
-    final dp = await startPostgres(
-      name: _kContainerName,
-      version: 'latest',
-      pgPort: 5432,
-      pgDatabase: 'postgres',
-      pgUser: 'postgres',
-      pgPassword: 'postgres',
-      cleanup: true,
-      configurations: [
-        // SSL settings
-        'ssl=on',
-        // The debian image includes a self-signed SSL cert that can be used:
-        'ssl_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem',
-        'ssl_key_file=/etc/ssl/private/ssl-cert-snakeoil.key',
-      ],
-      pgHbaConfPath: p.join(configPath, 'pg_hba.conf'),
-      postgresqlConfPath: p.join(configPath, 'postgresql.conf'),
+    await _startPostgresContainer(
+      port: port,
+      containerName: containerName,
     );
-
-    // Setup the database to support all kind of tests
-    // see _setupDatabaseStatements definition for details
-    for (final stmt in _setupDatabaseStatements) {
-      final args = [
-        'psql',
-        '-c',
-        stmt,
-        '-U',
-        'postgres',
-      ];
-      final res = await dp.exec(args);
-      if (res.exitCode != 0) {
-        final message =
-            'Failed to setup PostgreSQL database due to the following error:\n'
-            '${res.stderr}';
-        throw ProcessException(
-          'docker exec $_kContainerName',
-          args,
-          message,
-          res.exitCode,
-        );
-      }
-    }
   });
 
   tearDownAll(() async {
-    await Process.run('docker', ['stop', _kContainerName]);
+    await Process.run('docker', ['stop', containerName]);
   });
 }
 
-Future<bool> _isPostgresContainerRunning() async {
+Future<void> _startPostgresContainer({
+  required int port,
+  required String containerName,
+}) async {
+  final isRunning = await _isPostgresContainerRunning(containerName);
+  if (isRunning) {
+    return;
+  }
+
+  final configPath = p.join(Directory.current.path, 'test', 'pg_configs');
+
+  final dp = await startPostgres(
+    name: containerName,
+    version: 'latest',
+    pgPort: port,
+    pgDatabase: 'postgres',
+    pgUser: 'postgres',
+    pgPassword: 'postgres',
+    cleanup: true,
+    configurations: [
+      // SSL settings
+      'ssl=on',
+      // The debian image includes a self-signed SSL cert that can be used:
+      'ssl_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem',
+      'ssl_key_file=/etc/ssl/private/ssl-cert-snakeoil.key',
+    ],
+    pgHbaConfPath: p.join(configPath, 'pg_hba.conf'),
+    postgresqlConfPath: p.join(configPath, 'postgresql.conf'),
+  );
+
+  // Setup the database to support all kind of tests
+  // see _setupDatabaseStatements definition for details
+  for (final stmt in _setupDatabaseStatements) {
+    final args = [
+      'psql',
+      '-c',
+      stmt,
+      '-U',
+      'postgres',
+    ];
+    final res = await dp.exec(args);
+    if (res.exitCode != 0) {
+      final message =
+          'Failed to setup PostgreSQL database due to the following error:\n'
+          '${res.stderr}';
+      throw ProcessException(
+        'docker exec $containerName',
+        args,
+        message,
+        res.exitCode,
+      );
+    }
+  }
+}
+
+Future<bool> _isPostgresContainerRunning(String containerName) async {
   final pr = await Process.run(
     'docker',
     ['ps', '--format', '{{.Names}}'],
@@ -73,7 +84,7 @@ Future<bool> _isPostgresContainerRunning() async {
       .toString()
       .split('\n')
       .map((s) => s.trim())
-      .contains(_kContainerName);
+      .contains(containerName);
 }
 
 // This setup supports old and new test
