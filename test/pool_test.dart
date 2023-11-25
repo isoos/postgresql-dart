5def9889deffbcb99653b236f8cb645eed0418f1
István Soós <isoos@users.noreply.github.com>
2023-11-25 23:16:25 +0100

    adding the empty statement to the test

--- test/pool_test.dart
+++ test/pool_test.dart
@@ -135,6 +135,7 @@ void main() {
         ),
       );
 
+      await conn.execute('-- test'); // this doesn't throw but it causes the connection to close
       await db.execute('SELECT 1');
     });
   });
